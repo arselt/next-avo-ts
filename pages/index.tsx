2e09db269393559d62c761638a9fe3f4a845c73f
Arath Astorga <xarselt@gmail.com>
2022-03-18 21:45:18 -0600

    ✨👽 API working and connected with a product dynamic route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
 
-const Home = () => {
-    return(
-        <div>
-            <Navbar />
-            <h1>Hello Next.js ❎</h1>
-        </div>
-    )
+const HomePage = () => {
+  const [productList, setProductList] = useState<TProduct[]>([]);
+
+  useEffect(() => {
+    window
+      .fetch('api/avo')
+      .then((response) => response.json())
+      .then(({ data, length }) => {
+          setProductList(data)
+      })
+  }, [])
+
+  return (
+    <div>
+      <Navbar />
+      <h1>Hello Next.js ❎</h1>
+      {productList.map((product) => (
+        <li>
+          <a href={`/product/${product.id}`}>{product.name}</a>
+        </li>
+      ))}
+    </div>
+  );
 };
 
-export default Home;
+export default HomePage;
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router"
 
-const ProductItem = () => {
-    
+const ProductPage = () => {
+
+    const [product, setProduct] = useState<TProduct>();
+
     const {
-        query : { id }
+        query : { id },
     } = useRouter()
+    
+    useEffect(() => {
+        if (id) { // fetches only when id is given
+            window
+            .fetch(`/api/avo/${id}`)
+            .then((response) => response.json())
+            .then(( data ) => setProduct(data))
+        }
+    }, [id])
     
     return (
         <div>
-         esta es la pagina del producto: {id}   
+            <h2>esta es la pagina del producto: {product?.name}  </h2>
+            <p>{product?.attributes.description}</p>
         </div>
     )
 };
 
-export default ProductItem;
+export default ProductPage;
