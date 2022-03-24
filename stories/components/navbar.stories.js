3c9f43e9bc7248fd0bf13287d67b558dae921c39
Arath Astorga <xarselt@gmail.com>
2022-03-23 22:44:47 -0600

    ➕ Implemented Storybook

---
+++ stories/components/navbar.stories.js
@@ -0,0 +1,10 @@
+// /stories/components/navbar.stories.jsx
+
+import Navbar from '../../components/Navbar/index.tsx'
+
+export default {
+    title: "Components/Navbar",
+    component: Navbar,
+};
+
+export const NavbarComponent = () => <Navbar />
---
+++ stories/pages/home.stories.js
@@ -0,0 +1,10 @@
+// /stories/pages/home.stories.jsx
+
+import Home from "../../pages/index.tsx";
+
+export default {
+    title: "Pages/Home",
+    component: Home,
+};
+
+export const HomePage = () => <Home />
